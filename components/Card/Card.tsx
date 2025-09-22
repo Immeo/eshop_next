1005f07479a5bb2a48fdd415529c7ec0c96a0e3a
immeo <wayfarer245@gmail.com>
2025-09-22 15:52:08 +0300

    16-add css to Card

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,4 +1,3 @@
-import Image from 'next/image';
 import React from 'react';
 import styles from './Card.module.css';
 import { CardProps } from './Card.props';
@@ -7,21 +6,28 @@ export const Card = ({ product }: CardProps): React.JSX.Element => {
 	return (
 		<div>
 			<div className={styles.card}>
-				{product.images.map(image => (
-					<Image
-						key={image}
-						src={image}
-						width={200}
-						height={200}
-						priority={true}
-						alt={product.title}
-					/>
-				))}
+				<div className={styles.image}>
+					<img src={product.images[0]} alt={product.title} />
+				</div>
 				<div className={styles.info}>
 					<h3 className={styles.title}>{product.title}</h3>
-					<p className={styles.description}>{product.description}</p>
-					<span className={styles.price}>{product.price}</span>
+					{product.discountPercentage ? (
+						<div className={styles.prices}>
+							<span className={styles.discount}>
+								{product.discountPercentage}$
+							</span>
+							<span className={styles.oldPrice}>{product.price}$</span>
+						</div>
+					) : (
+						<span className={styles.price}>{product.price}$</span>
+					)}
 				</div>
+				<button type='button' className={styles.cart}>
+					Add to card
+				</button>
+				<button type='button' className={styles.buy}>
+					Buy now
+				</button>
 			</div>
 		</div>
 	);
